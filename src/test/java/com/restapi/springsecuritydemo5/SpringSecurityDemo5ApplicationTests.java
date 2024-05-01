0616967c0cdd92361caabc572d03b0b3e4d68edb
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-30 21:08:35 -0300

    criando projeto spring boot e spring security

---
+++ src/test/java/com/restapi/springsecuritydemo5/SpringSecurityDemo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.springsecuritydemo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
